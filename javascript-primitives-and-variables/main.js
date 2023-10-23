ef4304a36ec14a5cbd751e10801f888de677f845
Brendan Kormos <brendaaank@gmail.com>
2023-10-23 18:35:17 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+const fullName = 'Brendan Kormos';
+const isCool = true;
+const totalPets = 2;
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+
+console.log('fullName type:', typeof fullName);
+console.log('isCool type:', typeof isCool);
+console.log('totalPets type:', typeof totalPets);
+
+const unknown = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('never:', never);
+console.log('typeof never:', typeof never);
