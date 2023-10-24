95ad73b2eb0ad3409656b6472c8474db9aed84c7
Brendan Kormos <brendaaank@gmail.com>
2023-10-24 17:08:53 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,72 @@
+const orderHistory = [
+  {
+    orderNumber: '#113-2883177-2648248',
+    shipTo: 'JP Masher',
+    total: 138.93,
+    purchaseDate: '7.3.2020',
+    deliveryDate: '7.5.2020',
+    items: [
+      {
+        title:
+          'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        itemCost: 94.95,
+        returnBy: '8.4.2020',
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        itemCost: 33.99,
+        returnBy: '8.4.2020',
+      },
+    ],
+  },
+
+  {
+    orderNumber: '#114-2875557-9059409',
+    shipTo: 'JP Masher',
+    total: 17.22,
+    purchaseDate: '7.4.2020',
+    deliveryDate: '7.7.2020',
+    items: [
+      {
+        title:
+          'Gamecube Controller Adapter. :Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        itemCost: 15.98,
+        returnBy: '8.5.2020',
+      },
+    ],
+  },
+  {
+    orderNumber: '#113-9984268-1280257',
+    shipTo: 'JP Masher',
+    total: 44.53,
+    purchaseDate: '7.19.2020',
+    deliveryDate: '7.20.2020',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemCost: 41.33,
+        returnBy: '8.19.2020',
+      },
+    ],
+  },
+  {
+    orderNumber: '#114-3941689-8772232',
+    shipTo: 'JP Masher',
+    total: 34,
+    purchaseDate: '8.4.2020',
+    deliveryDate: '8.8.2020',
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemCost: 31.55,
+        returnBy: '9.7.2020',
+      },
+    ],
+  },
+];
+console.log(orderHistory);
