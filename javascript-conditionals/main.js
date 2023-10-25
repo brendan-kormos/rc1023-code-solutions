6fea5d0995a4e04d403793131c021442dce2a9ef
Brendan Kormos <brendaaank@gmail.com>
2023-10-25 18:20:38 +0000

    Completed javascript-conditionals

---
+++ javascript-conditionals/main.js
@@ -0,0 +1,77 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+
+// const person = {name: "Brendan Kormos", age: 21}
+
+function isUnderFive(number) {
+  return number < 5;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function startsWithJ(string) {
+  return string[0] === 'J';
+}
+
+function isOldEnoughToDrink(person) {
+  return person.age >= 21;
+}
+
+function isOldEnoughToDrive(person) {
+  return person.age >= 16;
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH < 7 && pH >= 0) {
+    return 'acid';
+  } else if (pH > 7 && pH <= 14) {
+    return 'base';
+  }
+  return 'invalid pH level';
+}
+
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+      return "We're the warner brothers!";
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+
+function recommendMovie(genre) {
+  switch (genre.toLowerCase()) {
+    case 'action':
+      return '57 Seconds';
+    case 'comedy':
+      return 'Old Dads';
+    case 'horror':
+      return 'Hereditary';
+    case 'drama':
+      return 'The Son';
+    case 'musical':
+      return 'Tick, Tick... Boom!';
+    case 'sci-fi':
+      return 'Blade Runner 2049';
+  }
+}
