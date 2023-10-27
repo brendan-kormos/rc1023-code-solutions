bb45d8d16ddd241cba742a7310a6cafd679c43b0
Brendan Kormos <brendaaank@gmail.com>
2023-10-27 23:50:05 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,7 +2,7 @@
 function chunk(array, size) {
   const newArray = [];
   let tempArray;
-  // does javascript have a function/method to just completely replace the need for what i've done below?
+  // is there another function/method to just completely replace what i've done below?
   array.forEach(function (element, index) {
     if (index % size === 0) {
       tempArray = [element];
