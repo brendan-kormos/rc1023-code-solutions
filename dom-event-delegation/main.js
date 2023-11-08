1ce6812accbc594fbdc3d4e6c5764faa47061252
Brendan Kormos <brendaaank@gmail.com>
2023-11-08 23:53:11 +0000

    Completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,12 @@
+const $taskList = document.querySelector('.task-list');
+
+$taskList.addEventListener('click', function (event) {
+  const $target = event.target;
+  const tagName = $target.tagName;
+  console.log('event target', $target);
+  console.log('event target tag name', tagName);
+
+  const $closest = $target.closest('.task-list-item');
+  console.log('closest element', $closest);
+  $closest.remove();
+});
