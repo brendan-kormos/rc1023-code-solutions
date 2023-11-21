a7fae209db96df1f205ee0461c15c3569ba3f2b4
Brendan Kormos <brendaaank@gmail.com>
2023-11-21 00:33:26 +0000

    working with every feature

---
+++ javascript-carousel/main.js
@@ -0,0 +1,68 @@
+const carouselImages$ = document.querySelectorAll('.carousel-image');
+const $carouselProgressContainer = document.querySelector('.carousel-progress');
+const carouselProgressCircles$ =
+  $carouselProgressContainer.querySelectorAll('.fa-circle');
+const $previous = document.querySelector('.previous');
+const $next = document.querySelector('.next');
+console.log(carouselProgressCircles$);
+console.log(typeof carouselProgressCircles$);
+
+let intervalID = null;
+let currentImage = 0;
+
+function resetInterval() {
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+  intervalID = setInterval(nextImage, 3 * 1000);
+}
+
+function setImage(number) {
+  resetInterval();
+  carouselImages$[currentImage].classList.add('hidden');
+  carouselProgressCircles$[currentImage].classList.add('far');
+  carouselProgressCircles$[currentImage].classList.remove('fas');
+  currentImage = number;
+  carouselImages$[currentImage].classList.remove('hidden');
+  carouselProgressCircles$[currentImage].classList.add('fas');
+  carouselProgressCircles$[currentImage].classList.remove('far');
+}
+
+function nextImage() {
+  resetInterval();
+  carouselImages$[currentImage].classList.add('hidden');
+  carouselProgressCircles$[currentImage].classList.add('far');
+  carouselProgressCircles$[currentImage].classList.remove('fas');
+  currentImage++;
+  if (currentImage > carouselImages$.length - 1) currentImage = 0;
+  carouselImages$[currentImage].classList.remove('hidden');
+  carouselProgressCircles$[currentImage].classList.add('fas');
+  carouselProgressCircles$[currentImage].classList.remove('far');
+}
+function previousImage() {
+  resetInterval();
+  carouselImages$[currentImage].classList.add('hidden');
+  carouselProgressCircles$[currentImage].classList.add('far');
+  carouselProgressCircles$[currentImage].classList.remove('fas');
+  currentImage--;
+  if (currentImage < 0) currentImage = carouselImages$.length - 1;
+  carouselImages$[currentImage].classList.remove('hidden');
+  carouselProgressCircles$[currentImage].classList.add('fas');
+  carouselProgressCircles$[currentImage].classList.remove('far');
+}
+
+resetInterval();
+
+$carouselProgressContainer.addEventListener('click', function (event) {
+  const $target = event.target;
+  if ($target.classList.contains('fa-circle')) {
+    for (let i = 0; i < carouselProgressCircles$.length; i++) {
+      const element = carouselProgressCircles$[i];
+      if (element === $target) {
+        setImage(i);
+      }
+    }
+  }
+});
+$next.addEventListener('click', nextImage);
+$previous.addEventListener('click', previousImage);
