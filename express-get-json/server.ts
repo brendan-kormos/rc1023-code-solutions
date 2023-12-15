52549fac2f14da9b0d2ec670ce3c2c3691b0d3aa
Brendan Kormos <brendaaank@gmail.com>
2023-12-15 07:17:21 +0000

    completed express-get-json

---
+++ express-get-json/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+const PORT = 8080;
+const app = express();
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.listen(PORT, () => {
+  console.log('listening on port:', PORT);
+});
+
+app.get('/api/grades', (req, res, next) => {
+  const array = [];
+  for (const key in grades) {
+    array.push(grades[key]);
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.json(array);
+  next();
+});
