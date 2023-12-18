cac308b2155f87386a222c98907aa9eb56322d26
Brendan Kormos <brendaaank@gmail.com>
2023-12-18 17:35:40 +0000

    a

--- express-post-json/server.ts
+++ express-post-json/server.ts
@@ -1,14 +1,30 @@
 import express from 'express';
+
+
 const app = express();
 const PORT = 8080;
 
 let nextID = 1;
+
 type Grade = {
   id: number;
   name: string;
   course: string;
   score: number;
 };
+
+type PostRequestGrade = {
+  name: string;
+  course: string;
+  score: number;
+};
+
+const templateRequestGrade: Record<string, string> = {
+  name: 'string',
+  course: 'string',
+  score: 'number',
+};
+
 const grades: Record<number, Grade> = {};
 
 app.listen(PORT, () => {
@@ -26,16 +42,26 @@ app.get('/api/grades', (req, res, next) => {
   next();
 });
 
+\
+
 app.post('/api/grades', (req, res, next) => {
-  const body = req.body;
-  const newObj = {
-    name: body.name,
-    score: body.score,
-    course: body.course,
-    id: nextID,
-  };
-  grades[newObj.id] = newObj;
+  const body: PostRequestGrade = req.body;
+  const newObj: Grade = { id: nextID, ...body };
+
+  // check data against template
+
+  for (const key in templateRequestGrade) {
+    const bodyValue = Number(body[key]) || body[key];
+    if ((typeof bodyValue as string) === templateRequestGrade[key]) {
+      newObj[key] = bodyValue;
+    } else {
+      res.status(400).json('Invalid data');
+      return;
+    }
+  }
+
+  grades[nextID] = newObj as Grade;
   nextID++;
-  res.status(201).send(newObj);
+  res.status(201).json(newObj);
   next();
 });
