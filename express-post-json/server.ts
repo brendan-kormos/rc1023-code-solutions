e5f4527a69fe7d5e0071ae3a9b9d8c8299767443
Brendan Kormos <brendaaank@gmail.com>
2023-12-18 23:30:47 +0000

    added check to make sure body was correct in api/grades post

--- express-post-json/server.ts
+++ express-post-json/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 
-
 const app = express();
 const PORT = 8080;
 
@@ -19,13 +18,13 @@ type PostRequestGrade = {
   score: number;
 };
 
-const templateRequestGrade: Record<string, string> = {
+const requestGradeSchema: Record<string, string> = {
   name: 'string',
   course: 'string',
   score: 'number',
 };
 
-const grades: Record<number, Grade> = {};
+const grades: Record<string, Grade> = {};
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
@@ -33,35 +32,42 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 
+function reconcileWithSchema(
+  obj1,
+  schema
+): [boolean, object] | [boolean, string] {
+  const newObj = {};
+  for (const key in schema) {
+    const bodyValue = Number(obj1[key]) || obj1[key];
+    if ((typeof bodyValue as string) === schema[key]) {
+      newObj[key] = bodyValue;
+    } else {
+      return [false, 'Invalid Data'];
+    }
+  }
+  return [true, newObj];
+}
+
 app.get('/api/grades', (req, res, next) => {
-  const array = [];
+  const array: Grade[] = [];
   for (const key in grades) {
+    console.log(key);
     array.push(grades[key]);
   }
   res.json(array);
   next();
 });
 
-\
-
 app.post('/api/grades', (req, res, next) => {
   const body: PostRequestGrade = req.body;
-  const newObj: Grade = { id: nextID, ...body };
-
-  // check data against template
-
-  for (const key in templateRequestGrade) {
-    const bodyValue = Number(body[key]) || body[key];
-    if ((typeof bodyValue as string) === templateRequestGrade[key]) {
-      newObj[key] = bodyValue;
-    } else {
-      res.status(400).json('Invalid data');
-      return;
-    }
+  console.log('hello', body);
+  const [success, value] = reconcileWithSchema(body, requestGradeSchema);
+  if (success) {
+    grades[nextID] = value as Grade;
+    nextID++;
+    res.status(201).json(value);
+    next();
+  } else {
+    res.status(500).json(value);
   }
-
-  grades[nextID] = newObj as Grade;
-  nextID++;
-  res.status(201).json(newObj);
-  next();
 });
