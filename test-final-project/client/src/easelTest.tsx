94ee4487b27994b880fb19713d15537f63a26147
Brendan Kormos <brendaaank@gmail.com>
2024-01-23 19:53:31 +0000

    moving pieces around works with any scaling

--- test-final-project/client/src/easelTest.tsx
+++ test-final-project/client/src/easelTest.tsx
@@ -1,6 +1,11 @@
 import * as createjs from '@thegraid/createjs-module';
 
+// test out individual features on specific scales, don't use everything at once
+
+// createjs.Ticker.timingMode = createjs.Ticker.RAF;
+
 const canvas = document.querySelector('#demoCanvas');
+
 const $container = document.querySelector('#canvas-container');
 
 const width = 800;
@@ -14,6 +19,12 @@ $container.style.height = height + 'px';
 stage.canvas.width = width;
 stage.canvas.height = height;
 
+function tick(event) {
+  stage.update(event);
+}
+createjs.Ticker.on('tick', tick);
+createjs.Ticker.framerate = 5;
+
 //working!
 // const shape = new createjs.Shape();
 // shape.graphics.beginFill('red').drawCircle(0, 0, 40);
@@ -23,14 +34,14 @@ stage.canvas.height = height;
 // end of working!
 
 //working
-// const myLine = new createjs.Shape();
-// myLine.graphics
-//   .setStrokeStyle(1)
-//   .beginStroke('red')
-//   .moveTo(0, 0)
-//   .lineTo(100, 200)
-//   .endStroke();
-// stage.addChild(myLine);
+const testLine = new createjs.Shape();
+testLine.graphics
+  .setStrokeStyle(1)
+  .beginStroke('red')
+  .moveTo(0, 0)
+  .lineTo(100, 700)
+  .endStroke();
+stage.addChild(testLine);
 //end of working
 
 const $button = document.createElement('div');
@@ -44,327 +55,232 @@ $container.prepend($button);
 
 const domElement = new createjs.DOMElement($button);
 stage.addChild(domElement);
-domElement.regX = 0;
 domElement.parent = stage;
-domElement.x = 300;
-domElement.y = 300;
-domElement.visible = true;
-// domElement.scale = .3
-stage.scale = 2;
+domElement.x = 25;
+domElement.y = 25;
+
 domElement.htmlElement.onclick = function () {
   console.log('clicked button!');
 };
-stage.update();
 
-// function ticking(event){
+function disableScroll() {
+  // Get the current page scroll position
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+  // if any scroll is attempted,
+  // set this to the previous value
+  window.onscroll = function () {
+    console.log(scrollTop);
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+disableScroll();
 
-// }
-// createjs.Ticker.addEventListener('tick', tick);
-// createjs.Ticker.on('tick', ticking);
-// createjs.Ticker.setFPS(60);
-// createjs.Ticker.addEventListener('tick', ticking)
+document.oncontextmenu = function () {
+  return false;
+};
 
-// stage.update();
+function factor() {
+  // multiply canvas screen cursor position by this
+  return 1 / stage.scale;
+}
 
-// console.log(stage);
+function scaledCursorPos(num: number) {
+  // if canvas width is 100, scale is .5, this will return 200
+  return num * factor();
+}
 
-// const context = canvas.getContext('2d');
+const drawings = [];
+const shapes = [];
 
-// document.oncontextmenu = function () {
-//   return false;
-// };
+// coordinates in the LOCAL canvas WORLD SPACE
+let localCursorX, localCursorY;
+let prevLocalCursorX, prevLocalCursorY;
 
-// const drawings = [];
+//screen coordinate relative to canvas origin point in SCREEN SPACE
+let cursorX, cursorY;
+let prevCursorX, prevCursorY;
 
-// // coordinates of our cursor
-// let cursorX;
-// let cursorY;
-// let prevCursorX;
-// let prevCursorY;
+function redrawCanvas() {
+  // set the canvas to the size of the window
+  // stage.canvas.width = document.body.clientWidth;
+  // stage.canvas.height = document.body.clientHeight;
 
-// // distance from origin
-// let offsetX = 0;
-// let offsetY = 0;
+  // context.fillStyle = '#fff';
+  // context.fillRect(0, 0, canvas.width, canvas.height);
 
-// // zoom amount
-// let scale = 1;
+  stage.drawRect = null;
+  for (let i = 0; i < drawings.length; i++) {
+    const line = drawings[i];
+    const myLine = shapes[i];
 
-// // convert coordinates
-// function toScreenX(xTrue) {
-//   return (xTrue + offsetX) * scale;
-// }
-// function toScreenY(yTrue) {
-//   return (yTrue + offsetY) * scale;
-// }
-// function toTrueX(xScreen) {
-//   return xScreen / scale - offsetX;
-// }
-// function toTrueY(yScreen) {
-//   return yScreen / scale - offsetY;
-// }
-// function trueHeight() {
-//   return canvas.clientHeight / scale;
-// }
-// function trueWidth() {
-//   return canvas.clientWidth / scale;
-// }
+    // myLine.graphics.beginStroke('blue').command
+    // myLine.graphics.setStrokeStyle(10).command
+    // myLine.graphics.moveTo(toScreenX(line.x0), toScreenY(line.y0)).command
+    // myLine.graphics.lineTo(toScreenX(line.x1), toScreenY(line.y1)).command
+    // myLine.graphics.endStroke().command
 
-// function redrawCanvas() {
-//   // set the canvas to the size of the window
-//   stage.canvas.width = document.body.clientWidth;
-//   stage.canvas.height = document.body.clientHeight;
-
-//   context.fillStyle = '#fff';
-//   // context.fillRect(0, 0, canvas.width, canvas.height);
-//   stage.update();
-//   // for (let i = 0; i < drawings.length; i++) {
-//   //   const line = drawings[i];
-//   //   drawLine(
-//   //     toScreenX(line.x0),
-//   //     toScreenY(line.y0),
-//   //     toScreenX(line.x1),
-//   //     toScreenY(line.y1)
-//   //   );
-//   // }
-
-// }
+    // drawLine(
+    //   toScreenX(line.x0),
+    //   toScreenY(line.y0),
+    //   toScreenX(line.x1),
+    //   toScreenY(line.y1)
+    // );
+  }
+  // stage.update();
+}
 // redrawCanvas();
 
-// // if the window changes size, redraw the canvas
-// window.addEventListener('resize', (event) => {
-//   redrawCanvas();
-// });
+// if the window changes size, redraw the canvas
+window.addEventListener('resize', (event) => {
+  // redrawCanvas();
+});
 
-// // Mouse Event Handlers
-// stage.canvas.addEventListener('mousedown', onMouseDown);
-// stage.canvas.addEventListener('mouseup', onMouseUp, false);
-// stage.canvas.addEventListener('mouseout', onMouseUp, false);
-// stage.canvas.addEventListener('mousemove', onMouseMove, false);
-// stage.canvas.addEventListener('wheel', onMouseWheel, false);
+function drawLine(x0, y0, x1, y1) {
+  const myLine = new createjs.Shape();
+  myLine.graphics
+    .setStrokeStyle(1)
+    .beginStroke('red')
+    .moveTo(x0, y0)
+    .lineTo(x1, y1)
+    .endStroke();
+  // console.log(1 + scale);
 
-// // Touch Event Handlers
-// stage.canvas.addEventListener('touchstart', onTouchStart);
-// stage.canvas.addEventListener('touchend', onTouchEnd);
-// stage.canvas.addEventListener('touchcancel', onTouchEnd);
-// stage.canvas.addEventListener('touchmove', onTouchMove);
+  // myLine.x = -stage.x * (1 + scale);
+  // myLine.y = -stage.y * (1 + scale);
+  return myLine;
+}
 
-// // mouse functions
-// let leftMouseDown = false;
-// let rightMouseDown = false;
-// function onMouseDown(event) {
-//   // detect left clicks
-//   if (event.button == 0) {
-//     leftMouseDown = true;
-//     rightMouseDown = false;
-//   }
-//   // detect right clicks
-//   if (event.button == 2) {
-//     rightMouseDown = true;
-//     leftMouseDown = false;
-//   }
+// mouse functions
+let leftMouseDown = false;
+let rightMouseDown = false;
+function onMouseDown(event) {
+  // detect left clicks
+  if (event.button == 0) {
+    leftMouseDown = true;
+    rightMouseDown = false;
+  }
 
-//   // update the cursor coordinates
-//   const rect = event.target.getBoundingClientRect();
+  // detect right clicks
+  if (event.button == 2) {
+    rightMouseDown = true;
+    leftMouseDown = false;
+  }
 
-//   cursorX = event.clientX - rect.left; //x position within the element.
-//   cursorY = event.clientY - rect.top; //y position within the element.
+  const rect = event.target.getBoundingClientRect(); //event.target is canvas
 
-//   // cursorX = event.pageX;
-//   // cursorY = event.pageY;
+  cursorX = event.clientX - rect.x; //x screen position relative to canvas.
+  cursorY = event.clientY - rect.y; //y screen position relative to canvas.
 
-//   prevCursorX = event.clientX - rect.left; //x position within the element.
-//   prevCursorY = event.clientY - rect.top; //y position within the element.
-//   // prevCursorX = event.pageX;
-//   // prevCursorY = event.pageY;
-// }
-// function onMouseMove(event) {
-//   // get mouse position
+  const { x, y } = stage.globalToLocal(cursorX, cursorY); // real position regardless of scale
+  localCursorX = x;
+  localCursorY = y;
 
-//   const rect = event.target.getBoundingClientRect();
+  prevLocalCursorX = localCursorX;
+  prevLocalCursorY = localCursorY;
+  prevCursorX = cursorX;
+  prevCursorY = cursorY;
+}
 
-//   cursorX = event.clientX - rect.left; //x position within the element.
-//   cursorY = event.clientY - rect.top; //y position within the element.
+stage.scale = 0.15;
+let foundNan = false;
+function onMouseMove(event) {
+  // get mouse position
 
-//   // const x = event.pageX - event.currentTarget.offsetLeft;
-//   // const y = event.pageY - event.currentTarget.offsetTop;
-//   // console.log('x', x, 'y', y);
+  const rect = event.target.getBoundingClientRect(); //event.target is canvas
+  cursorX = event.clientX - rect.x; //x screen position relative to canvas.
+  cursorY = event.clientY - rect.y; //y screen position relative to canvas.
 
-//   // cursorX = event.x;
-//   // cursorY = event.y;
-//   // console.log(cursorX);
-//   const scaledX = toTrueX(cursorX);
-//   const scaledY = toTrueY(cursorY);
-//   const prevScaledX = toTrueX(prevCursorX);
-//   const prevScaledY = toTrueY(prevCursorY);
+  // num to multiply by to get to normal scale
+  const scaledX = scaledCursorPos(cursorX);
+  const scaledY = scaledCursorPos(cursorY);
 
-//   if (leftMouseDown) {
-//     // add the line to our drawing history
-//     drawings.push({
-//       x0: prevScaledX,
-//       y0: prevScaledY,
-//       x1: scaledX,
-//       y1: scaledY,
-//     });
-//     // draw a line
-//     drawLine(prevCursorX, prevCursorY, cursorX, cursorY);
-//   }
-//   if (rightMouseDown) {
-//     // move the screen
-//     offsetX += (cursorX - prevCursorX) / scale;
-//     offsetY += (cursorY - prevCursorY) / scale;
-//     redrawCanvas();
-//   }
-//   prevCursorX = cursorX;
-//   prevCursorY = cursorY;
-// }
-// function onMouseUp() {
-//   leftMouseDown = false;
-//   rightMouseDown = false;
-// }
-// function onMouseWheel(event) {
-//   const deltaY = event.deltaY;
-//   const scaleAmount = -deltaY / 500;
-//   scale = scale * (1 + scaleAmount);
+  const obj = stage.globalToLocal(cursorX, cursorY); // real position regardless of scale
+  const { x, y } = obj;
+  localCursorX = x;
+  localCursorY = y;
 
-//   // zoom the page based on where the cursor is
+  if (leftMouseDown) {
+    // add the line to our drawing history
+    console.log('drawing new line!', x, y);
+    drawings.push({
+      x0: prevLocalCursorX,
+      y0: prevLocalCursorY,
+      x1: localCursorX,
+      y1: localCursorY,
+    });
+    // draw a line
+    shapes.push(
+      stage.addChild(
+        drawLine(prevLocalCursorX, prevLocalCursorY, localCursorX, localCursorY)
+      )
+    );
+  }
 
-//   const rect = event.target.getBoundingClientRect();
+  const xDiff = cursorX - prevCursorX
+  const yDiff = cursorY - prevCursorY
+  const scaledXDiff = scaledCursorPos(cursorX) - scaledCursorPos(prevCursorX);
+  const scaledYDiff = scaledCursorPos(cursorY) - scaledCursorPos(prevCursorY)
+  if (rightMouseDown) {
+    if (!foundNan) {
+      if (isNaN(x)) {
+        foundNan = !foundNan;
+      }
+      console.log('scaledX', scaledX);
+      console.log('scaledXDiff', scaledXDiff)
+      console.log('stageOffset', stage.x)
+      // console.log('prevLocalPos', prevLocalCursorX, prevLocalCursorY);
+      // console.log('localPos', localCursorX, localCursorY);
+      // console.log('xSubtracted', localCursorX - prevLocalCursorX);
+    }
 
-//   cursorX = event.clientX - rect.left; //x position within the element.
-//   cursorY = event.clientY - rect.top; //y position within the element.
+    // console.log(localCursorX, prevLocalCursorX)
+    // console.log('xDiff', localCursorX - prevLocalCursorX)
+    stage.x += xDiff;
+    stage.y += yDiff;
+  }
 
-//   const distX = cursorX / canvas.clientWidth;
-//   const distY = cursorY / canvas.clientHeight;
+  prevLocalCursorX = localCursorX;
+  prevLocalCursorY = localCursorY;
+  prevCursorX = cursorX;
+  prevCursorY = cursorY;
+}
 
-//   // calculate how much we need to zoom
-//   const unitsZoomedX = trueWidth() * scaleAmount;
-//   const unitsZoomedY = trueHeight() * scaleAmount;
+function onMouseUp() {
+  leftMouseDown = false;
+  rightMouseDown = false;
+}
 
-//   const unitsAddLeft = unitsZoomedX * distX;
-//   const unitsAddTop = unitsZoomedY * distY;
+// Mouse Event Handlers
 
-//   offsetX -= unitsAddLeft;
-//   offsetY -= unitsAddTop;
+stage.canvas.addEventListener('mousedown', onMouseDown);
+stage.canvas.addEventListener('mouseup', onMouseUp, false);
+stage.canvas.addEventListener('mouseout', onMouseUp, false); // unclick mouse if out of canvas
+stage.canvas.addEventListener('mousemove', onMouseMove, false);
 
-//   redrawCanvas();
-// }
+// stage.addEventListener('wheel', onMouseWheel, false);
 
-// function drawLine(x0, y0, x1, y1) {
-//   const g = new createjs.Graphics();
-//   console.log('drawingLine')
-//   g.setStrokeStyle(1)
-//   g.beginStroke('#000000');
-//   g.beginFill("red")
-//   g.moveTo(x0, y1)
-//   g.lineTo(x1,y1)
-//   g.endStroke()
-
-//   // context.beginPath();
-//   // context.moveTo(x0, y0);
-//   // context.lineTo(x1, y1);
-//   // context.strokeStyle = '#000';
-//   // context.lineWidth = 2;
-//   // context.stroke();
-// }
-
-// // touch functions
-// const prevTouches = [null, null]; // up to 2 touches
-// let singleTouch = false;
-// let doubleTouch = false;
-// function onTouchStart(event) {
-//   if (event.touches.length == 1) {
-//     singleTouch = true;
-//     doubleTouch = false;
-//   }
-//   if (event.touches.length >= 2) {
-//     singleTouch = false;
-//     doubleTouch = true;
-//   }
-
-//   // store the last touches
-//   prevTouches[0] = event.touches[0];
-//   prevTouches[1] = event.touches[1];
-// }
-// function onTouchMove(event) {
-//   // get first touch coordinates
-//   // TODO: copy cursorX and cursorY from other computer functions
-//   const touch0X = event.touches[0].pageX;
-//   const touch0Y = event.touches[0].pageY;
-//   const prevTouch0X = prevTouches[0].pageX;
-//   const prevTouch0Y = prevTouches[0].pageY;
-
-//   const scaledX = toTrueX(touch0X);
-//   const scaledY = toTrueY(touch0Y);
-//   const prevScaledX = toTrueX(prevTouch0X);
-//   const prevScaledY = toTrueY(prevTouch0Y);
-
-//   if (singleTouch) {
-//     // add to history
-//     drawings.push({
-//       x0: prevScaledX,
-//       y0: prevScaledY,
-//       x1: scaledX,
-//       y1: scaledY,
-//     });
-//     drawLine(prevTouch0X, prevTouch0Y, touch0X, touch0Y);
-//   }
-
-//   if (doubleTouch) {
-//     // get second touch coordinates
-//     const touch1X = event.touches[1].pageX;
-//     const touch1Y = event.touches[1].pageY;
-//     const prevTouch1X = prevTouches[1].pageX;
-//     const prevTouch1Y = prevTouches[1].pageY;
-
-//     // get midpoints
-//     const midX = (touch0X + touch1X) / 2;
-//     const midY = (touch0Y + touch1Y) / 2;
-//     const prevMidX = (prevTouch0X + prevTouch1X) / 2;
-//     const prevMidY = (prevTouch0Y + prevTouch1Y) / 2;
-
-//     // calculate the distances between the touches
-//     const hypot = Math.sqrt(
-//       Math.pow(touch0X - touch1X, 2) + Math.pow(touch0Y - touch1Y, 2)
-//     );
-//     const prevHypot = Math.sqrt(
-//       Math.pow(prevTouch0X - prevTouch1X, 2) +
-//         Math.pow(prevTouch0Y - prevTouch1Y, 2)
-//     );
-
-//     // calculate the screen scale change
-//     const zoomAmount = hypot / prevHypot;
-//     scale = scale * zoomAmount;
-//     const scaleAmount = 1 - zoomAmount;
-
-//     // calculate how many pixels the midpoints have moved in the x and y direction
-//     const panX = midX - prevMidX;
-//     const panY = midY - prevMidY;
-//     // scale this movement based on the zoom level
-//     offsetX += panX / scale;
-//     offsetY += panY / scale;
-
-//     // Get the relative position of the middle of the zoom.
-//     // 0, 0 would be top left.
-//     // 0, 1 would be top right etc.
-//     const zoomRatioX = midX / canvas.clientWidth;
-//     const zoomRatioY = midY / canvas.clientHeight;
-
-//     // calculate the amounts zoomed from each edge of the screen
-//     const unitsZoomedX = trueWidth() * scaleAmount;
-//     const unitsZoomedY = trueHeight() * scaleAmount;
-
-//     const unitsAddLeft = unitsZoomedX * zoomRatioX;
-//     const unitsAddTop = unitsZoomedY * zoomRatioY;
-
-//     offsetX += unitsAddLeft;
-//     offsetY += unitsAddTop;
-
-//     redrawCanvas();
-//   }
-//   prevTouches[0] = event.touches[0];
-//   prevTouches[1] = event.touches[1];
-// }
-// function onTouchEnd(event) {
-//   singleTouch = false;
-//   doubleTouch = false;
-// }
+function onMouseWheel(event) {
+  //   const deltaY = event.deltaY;
+  //   console.log(deltaY);
+  //   const scaleAmount = -deltaY / 500;
+  //   scale = scale * (1 + scaleAmount);
+  //   stage.scale = scale;
+  //   // zoom the page based on where the cursor is
+  //   const rect = event.target.getBoundingClientRect();
+  //   cursorX = event.clientX - rect.left; //x position within the element.
+  //   cursorY = event.clientY - rect.top; //y position within the element.
+  //   const distX = cursorX / canvas.clientWidth;
+  //   const distY = cursorY / canvas.clientHeight;
+  //   // calculate how much we need to zoom
+  //   const unitsZoomedX = trueWidth() * scaleAmount;
+  //   const unitsZoomedY = trueHeight() * scaleAmount;
+  //   const unitsAddLeft = unitsZoomedX * distX;
+  //   const unitsAddTop = unitsZoomedY * distY;
+  //   offsetX -= unitsAddLeft;
+  //   offsetY -= unitsAddTop;
+  //   // stage.x = offsetX;
+  //   // stage.y = offsetY;
+  // //
+  //   // redrawCanvas();
+}
